72ca27235e28c39c08738abc76ddba1fa93758c9
JovanovicLela <jovanovic.lela.00@gmail.com>
2023-03-10 23:34:10 +0100

    mapper update

--- src/main/java/com/doc/auth/AuthApplication.java
+++ src/main/java/com/doc/auth/AuthApplication.java
@@ -1,7 +1,9 @@
 package com.doc.auth;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableMongoAuditing
@@ -11,5 +13,6 @@ public class AuthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/doc/auth/config/BeanConfig.java
+++ src/main/java/com/doc/auth/config/BeanConfig.java
@@ -1,6 +1,9 @@
 package com.doc.auth.config;
 
+import com.doc.auth.dtos.UserInfoDTO;
+import com.doc.auth.models.MarkdownUserModel;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,7 +13,14 @@ public class BeanConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        final TypeMap<MarkdownUserModel, UserInfoDTO> markdownUserModelUserInfoDTOTypeMap =  modelMapper.typeMap(MarkdownUserModel.class, UserInfoDTO.class);
+       // markdownUserModelUserInfoDTOTypeMap.addMappings(mapping -> mapping.skip(UserInfoDTO::setPassword));
+
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+        return modelMapper;
     }
 
     @Bean
