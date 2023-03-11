394ec3b1758e44148c5440e4fff36cc2605f2fc6
JovanovicLela <jovanovic.lela.00@gmail.com>
2023-03-11 21:28:20 +0100

    added cors filter

--- src/main/java/com/doc/auth/config/BeanConfig.java
+++ src/main/java/com/doc/auth/config/BeanConfig.java
@@ -6,7 +6,11 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class BeanConfig {
@@ -26,5 +30,22 @@ public class BeanConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.applyPermitDefaultValues();
+
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        config.addAllowedMethod(HttpMethod.OPTIONS);
+
+        configSource.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(configSource);
     }
 }
